73f2288bafd7327dd33781c19d349594dafb2418
Wesley Hendrikx <wesleyhendrikx@hotmail.com>
2024-04-19 15:45:39 +0200

    add docker container support
    Some students are having trouble with running MS SQL Server LocalDb (e.g. on virtual machine with ARM processor).
    Therfore docker support is added to start the api in a Linux docker container using docker compose.
    A second container contains the database.

--- Backend/Onitama.Bootstrapper/WebApplicationExtensions.cs
+++ Backend/Onitama.Bootstrapper/WebApplicationExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Onitama.Infrastructure;
+using Polly;
 
 namespace Onitama.Bootstrapper;
 
@@ -10,6 +13,24 @@ public static class WebApplicationExtensions
     {
         var scope = app.Services.CreateScope();
         OnitamaDbContext context = scope.ServiceProvider.GetRequiredService<OnitamaDbContext>();
-        context.Database.EnsureCreated();
+        ILogger logger = scope.ServiceProvider.GetRequiredService<ILogger<WebApplication>>();
+        try
+        {
+            //if the sql server container is not created on run docker compose this migration can't fail for network related exception.
+            var retry = Policy.Handle<SqlException>()
+                .WaitAndRetry(new TimeSpan[]
+                {
+                    TimeSpan.FromSeconds(3),
+                    TimeSpan.FromSeconds(5),
+                    TimeSpan.FromSeconds(8),
+                });
+            retry.Execute(() => context.Database.EnsureCreated());
+
+            logger.LogInformation("Created database");
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occurred while creating the database");
+        }
     }
 }
