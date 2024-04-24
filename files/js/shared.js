c427641eda84360215a9b3b61b404f31f5e7f8c0
TeoPXL <TeoPXL@users.noreply.github.com>
2024-04-24 14:07:57 +0000

    deploy: 4766331b03950f7d0f513ec79609059f17fd2f4c

--- files/js/shared.js
+++ files/js/shared.js
@@ -2,6 +2,7 @@ const localApi = "https://localhost:5051";
 const remoteApi = "https://onitama61.azurewebsites.net";
 const chosenApi = JSON.parse(localStorage.getItem("api"));
 const user = JSON.parse(localStorage.getItem("user"));
+
 let currentApi = "";
 let localApiExists = false;
 let remoteApiExists = false;
