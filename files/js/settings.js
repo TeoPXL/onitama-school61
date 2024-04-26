15289e75b4f872c44ecdf01e8903191da291d812
TeoPXL <TeoPXL@users.noreply.github.com>
2024-04-26 19:22:34 +0000

    deploy: f537782c6501330e8184ecaba83425db306614f8

---
+++ files/js/settings.js
@@ -0,0 +1,28 @@
+const settingInputs = document.querySelectorAll('.setting-input');
+settingInputs.forEach(element => {
+    switch (element.getAttribute('onitama-setting')) {
+        case 'force-remote-api':
+            if(userSettings['force-remote-api'] == 'true'){
+                element.checked = true;
+            }
+            break;
+        
+        case 'simulate-offline-mode':
+            if(userSettings['simulate-offline-mode'] == 'true'){
+                element.checked = true;
+            }
+            break;
+
+        case 'suppress-api-errors':
+            if(userSettings['suppress-api-errors'] == 'true'){
+                element.checked = true;
+            }
+            break;
+        default:
+            break;
+    }
+});
+settingInputs.forEach(element => element.addEventListener('click', () => {
+    const setting = element.getAttribute("onitama-setting");
+    localStorage.setItem(setting, element.checked);
+}));
