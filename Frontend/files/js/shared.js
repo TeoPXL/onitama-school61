4aef972ededdb2b7425e46ed971440721ee1cd9d
TeoPXL <teoman.akgun@student.pxl.be>
2024-04-26 17:15:03 +0200

    Frontend error handling changes

--- Frontend/files/js/shared.js
+++ Frontend/files/js/shared.js
@@ -9,11 +9,12 @@ let currentApi = "";
 let localApiExists = false;
 let remoteApiExists = false;
 
-function throw_floating_error(error, code){
+function throw_floating_error(error, code, color){
     if(!floatingError.classList.contains('floating-error-hidden')){
         return;
     }
     console.log(error);
+    floatingError.style.background = color;
     floatingError.querySelector('.floating-error-title').textContent = "Error: " + code;
     floatingError.querySelector('.floating-error-subtitle').textContent = error;
     floatingError.classList.remove('floating-error-hidden');
@@ -23,7 +24,7 @@ floatingError.querySelector('.floating-error-button').addEventListener('click', 
     floatingError.classList.add('floating-error-hidden');
 });
 
-fetch(localApi)
+fetch(localApi+"/ping")
     .then(response => {
         if (response.ok) {
             localApiExists = true;
@@ -33,7 +34,7 @@ fetch(localApi)
         console.error("Error while trying to reach local API:", error);
     });
 
-fetch(remoteApi)
+fetch(remoteApi+"/ping")
     .then(response => {
         if (response.ok) {
             remoteApiExists = true;
@@ -44,7 +45,7 @@ fetch(remoteApi)
     });
     setTimeout(() => {
         if(localApiExists == false && remoteApiExists == false){
-            throw_floating_error('Could not make a connection to both local and remote API! Try waiting for a cold start.', '504');
+            throw_floating_error('Could not make a connection to both local and remote API! Try waiting for a cold start. You can dismiss this message if you intend to play offline.', '504', '#303031');
         }
     }, 2000);
     
