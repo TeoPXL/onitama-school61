69ac16c17f47c1f8e1f9bed4bab5833762ab8e93
TeoPXL <teoman.akgun@student.pxl.be>
2024-04-29 05:55:11 +0200

    Backend api endpoint fix

--- Backend/Onitama.Core/TableAggregate/Contracts/ITableRepository.cs
+++ Backend/Onitama.Core/TableAggregate/Contracts/ITableRepository.cs
@@ -26,5 +26,10 @@ namespace Onitama.Core.TableAggregate.Contracts
         /// Finds all the tables in storage that have a seat available.
         /// </summary>
         IList<ITable> FindTablesWithAvailableSeats();
+
+        /// <summary>
+        /// Finds all the tables.
+        /// </summary>
+        IList<ITable> FindTables();
     }
 }
