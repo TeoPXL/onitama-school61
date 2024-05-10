6fd7643a044798826aec308e51a8641f5f178ba1
TeoPXL <teoman.akgun@student.pxl.be>
2024-05-10 19:15:59 +0200

    Backend GameService cleanup

--- Backend/Onitama.Core/GameAggregate/GameService.cs
+++ Backend/Onitama.Core/GameAggregate/GameService.cs
@@ -18,27 +18,20 @@ internal class GameService : IGameService
     public IGame GetGame(Guid gameId)
     {
         return _gameRepository.GetById(gameId);
-        //throw new NotImplementedException("TODO: use the constructor-injected repository to retrieve a game");
     }
 
     public IReadOnlyList<IMove> GetPossibleMovesForPawn(Guid gameId, Guid playerId, Guid pawnId, string moveCardName)
     {
         return _gameRepository.GetById(gameId).GetPossibleMovesForPawn(playerId, pawnId, moveCardName);
-        //throw new NotImplementedException(
-        //    "TODO: use the constructor-injected repository to retrieve the game and get the possible moves of that game");
     }
 
     public void MovePawn(Guid gameId, Guid playerId, Guid pawnId, string moveCardName, ICoordinate to)
     {
         _gameRepository.GetById(gameId).MovePawn(playerId, pawnId, moveCardName, to);
-        //throw new NotImplementedException(
-        //    "TODO: use the constructor-injected repository to retrieve the game and get move a pawn of that game");
     }
 
     public void SkipMovementAndExchangeCard(Guid gameId, Guid playerId, string moveCardName)
     {
         _gameRepository.GetById(gameId).SkipMovementAndExchangeCard(playerId, moveCardName);
-        //throw new NotImplementedException(
-        //    "TODO: use the constructor-injected repository to retrieve the game and skip a movement for that game");
     }
 }
