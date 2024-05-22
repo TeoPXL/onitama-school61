4277b0b9e9d41b8f0b3759f079afe5345757f85e
MathiasSteegmansPXL <MathiasSteegmansPXL@users.noreply.github.com>
2024-05-22 13:06:50 +0000

    deploy: 47f301cf599e4da8eafd01de1b0c8d8f951a6785

--- files/js/settings.js
+++ files/js/settings.js
@@ -1,40 +1,48 @@
-const settingInputs = document.querySelectorAll('.setting-input');
-settingInputs.forEach(element => {
-    switch (element.getAttribute('onitama-setting')) {
-        case 'force-remote-api':
-            if(userSettings['force-remote-api'] == 'true'){
-                element.checked = true;
-            }
-            break;
+document.addEventListener('DOMContentLoaded', function() {
+    const settingInputs = document.querySelectorAll('.setting-input');
 
-        case 'force-dev-api':
-            if(userSettings['force-dev-api'] == 'true'){
-                element.checked = true;
-            }
-            break;
-        
-        case 'simulate-offline-mode':
-            if(userSettings['simulate-offline-mode'] == 'true'){
-                element.checked = true;
-            }
-            break;
+    settingInputs.forEach(element => {
+        const setting = element.getAttribute('onitama-setting');
+        const storedSetting = localStorage.getItem(setting);
+        switch (setting) {
+            case 'force-remote-api':
+                element.checked = storedSetting === 'true';
+                break;
+            case 'force-dev-api':
+                element.checked = storedSetting === 'true';
+                break;
+            case 'simulate-offline-mode':
+                element.checked = storedSetting === 'true';
+                break;
+            case 'suppress-api-errors':
+                element.checked = storedSetting === 'true';
+                break;
+            case 'suppress-all-errors':
+                element.checked = storedSetting === 'true';
+                break;
+            case 'toggle-lightmode':
+                element.checked = storedSetting === 'true';
+                applyTheme();
+                break;
+        }
+    });
 
-        case 'suppress-api-errors':
-            if(userSettings['suppress-api-errors'] == 'true'){
-                element.checked = true;
-            }
-            break;
+    settingInputs.forEach(element => element.addEventListener('click', () => {
+        const setting = element.getAttribute("onitama-setting");
+        localStorage.setItem(setting, element.checked.toString());
 
-        case 'suppress-all-errors':
-            if(userSettings['suppress-all-errors'] == 'true'){
-                element.checked = true;
-            }
-            break;
-        default:
-            break;
-    }
+        if (setting === 'toggle-lightmode') {
+            applyTheme();
+        }
+    }));
 });
-settingInputs.forEach(element => element.addEventListener('click', () => {
-    const setting = element.getAttribute("onitama-setting");
-    localStorage.setItem(setting, element.checked);
-}));
+
+function applyTheme() {
+    const themeClass = localStorage.getItem('toggle-lightmode') === 'true' ? 'light' : 'dark';
+    document.documentElement.className = themeClass;
+}
+
+
+
+
+
